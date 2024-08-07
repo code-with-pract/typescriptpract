e6803dbb87ccd337a524a918970168aa3a0a4c61
sanjeev kushwaha <sanjeevkushwaha876@gmail.com>
2024-08-08 00:27:47 +0530

    added more changes and files

--- pract3/myfunction.ts
+++ pract3/myfunction.ts
@@ -5,8 +5,15 @@
 
 // addSum("5");
 
-function addSum(num: number) {
+// function addSum(num: number) {
+// //   return num + 2;
+//   return "Hello"
+// }
+
+// better way to write function
+function addSum(num: number): number {
   return num + 2;
+  //   return "Hello";
 }
 
 function getUpper(val: string) {
@@ -18,6 +25,38 @@ getUpper("namami devi narmade");
 
 function signUpUser(name: string, email: string, isPaid: boolean) {}
 
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
 signUpUser("sanjeev", "sanjeev@gmail.com", false);
+
+loginUser("sanju", "baba@gmail.com");
+
+// function getValue(myVal: number) : boolean {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return "200 OK";
+// }
+
+// Better way to write the function
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["Thor", "Spiderman", "Ironman"];
+// const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+  return `Hero is ${hero}`;
+});
+
+function consoleError(errmeg: string): void {
+  console.log(errmeg);
+}
+
+function errorHandler(errmsg: string): never {
+  throw new Error(errmsg);
+}
+
 
 export {};
---
+++ pract3/myobjects.js
@@ -0,0 +1,14 @@
+"use strict";
+// let User = {
+//   name: "Sanju baba",
+//   email: "baba@gmail.com",
+//   isActive: true,
+// };
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+    _id: "123",
+    name: "sanju",
+    email: "sanju@yahoo.com",
+    isActive: false,
+};
+myUser.email = "baba@gmail.com";
