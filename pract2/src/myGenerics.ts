547bcedad56698b782c02202afc2271449a8a4d4
sanjeev kushwaha <sanjeevkushwaha876@gmail.com>
2024-08-11 00:10:12 +0530

    added detection topic

--- pract2/src/myGenerics.ts
+++ pract2/src/myGenerics.ts
@@ -28,3 +28,56 @@ identityFour<Bottle>({
   brand: "Honda",
   type: 12,
 });
+
+//
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchProdcuts = <T>(prodducts: T[]): T => {
+  // do some database operations
+  const myIndex = 4;
+  return prodducts[myIndex];
+};
+
+//////
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+anotherFunction(3, {
+  connection: "tnpLab.in",
+  username: "shubham",
+  password: "tnp@123",
+});
+
+//////////
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  auther: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+
+  addCart(product: T) {
+    this.cart.push(product);
+  }
+}
